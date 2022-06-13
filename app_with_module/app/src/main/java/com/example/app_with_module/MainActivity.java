daa94fa50bfcb2862ac616db34ae0ea24aeac0e3
JoaoMendonca1992 <101359165+JoaoMendonca1992@users.noreply.github.com>
2022-06-13 14:03:01 -0300

    Ajuste main

--- app_with_module/app/src/main/java/com/example/app_with_module/MainActivity.java
+++ app_with_module/app/src/main/java/com/example/app_with_module/MainActivity.java
@@ -1,24 +1,12 @@
 package com.example.app_with_module;
-
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.app_with_module.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
